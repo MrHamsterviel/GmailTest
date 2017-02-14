df7a37c5893a7dfecc4b7c568242ebfcb1b14f74
Roman <Romanv994@gmail.com>
2017-02-14 11:31:53 +0300

    TestParams changed to struct

--- AcumaticaTask/AcumaticaTask/TestParams.cs
+++ AcumaticaTask/AcumaticaTask/TestParams.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AcumaticaTask
+﻿namespace AcumaticaTask
 {
-    public class TestParams
+    public struct TestParams
     {
         public string _BrowserName;
         public string _UserName;
         public string _Passwd;
         public string _MsgReciever;
-
-        public TestParams()
-        {
-            _BrowserName = "0";
-            _UserName = "0";
-            _Passwd = "0";
-            _MsgReciever = "0";
-
-        }
     }
 }
